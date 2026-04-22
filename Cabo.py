331726e2568ed938f39059ed1500760b00b42609
maudelasjavador <mauricio.santosbritto@gmail.com>
2026-04-22 20:03:53 -0300

    adição das texturas do cabo de madeira escura, e tampas

--- Cabo.py
+++ Cabo.py
@@ -24,22 +24,54 @@ class Cabo:
             self.vertices.append((x, self.altura / 2, z))
 
     def draw(self):
-        glColor3f(0.7, 0.7, 0.7)
-        glBegin(GL_LINES)
+        glColor3f(1, 1, 1)  # mesma coisa da haste, só n apaga pra n explodir
 
-        # base
+        # texturização dos lado
+        glBegin(GL_QUADS)
+
         for i in range(self.lados):
-            glVertex3fv(self.vertices[i])
-            glVertex3fv(self.vertices[(i + 1) % self.lados])
+            p1 = self.vertices[i]
+            p2 = self.vertices[(i + 1) % self.lados]
+            p3 = self.vertices[(i + 1) % self.lados + self.lados]
+            p4 = self.vertices[i + self.lados]
 
-        # topo
+            u1 = i / self.lados
+            u2 = (i + 1) / self.lados
+
+            glTexCoord2f(u1, 0)
+            glVertex3fv(p1)
+
+            glTexCoord2f(u2, 0)
+            glVertex3fv(p2)
+
+            glTexCoord2f(u2, 1)
+            glVertex3fv(p3)
+
+            glTexCoord2f(u1, 1)
+            glVertex3fv(p4)
+
+        glEnd()
+
+        # tampa de cima
+        glBegin(GL_POLYGON)
         for i in range(self.lados):
-            glVertex3fv(self.vertices[i + self.lados])
-            glVertex3fv(self.vertices[((i + 1) % self.lados) + self.lados])
+            x, y, z = self.vertices[i]
 
-        # laterais
+            u = (cos(2 * pi * i / self.lados) + 1) / 2
+            v = (sin(2 * pi * i / self.lados) + 1) / 2
+
+            glTexCoord2f(u, v)
+            glVertex3f(x, y, z)
+        glEnd()
+
+        # tampa de baixo
+        glBegin(GL_POLYGON)
         for i in range(self.lados):
-            glVertex3fv(self.vertices[i])
-            glVertex3fv(self.vertices[i + self.lados])
+            x, y, z = self.vertices[i + self.lados]
 
+            u = (cos(2 * pi * i / self.lados) + 1) / 2
+            v = (sin(2 * pi * i / self.lados) + 1) / 2
+
+            glTexCoord2f(u, v)
+            glVertex3f(x, y, z)
         glEnd()
